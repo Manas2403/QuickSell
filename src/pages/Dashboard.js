570bd71864b67280ea5a4ef0825e381f363cb45d
guptamanas149@gmail.com <guptamanas149@gmail.com>
2023-09-23 00:19:44 +0530

    initial

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect, useContext } from "react";
+import Card from "../components/Card";
+export default function Dashboard() {
+    return (
+        <>
+            <Card />
+        </>
+    );
+}
